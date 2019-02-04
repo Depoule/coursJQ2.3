2b29d2631f4005af466931fe3e8551d37a6c1a45
Depoule <jpoulet02100@gmail.com>
2019-02-04 16:29:37 +0100

    fini et corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// mise en place d'une fonction hide et une show pour afficher ou cacher le texte
+$(document).ready(function(){
+  $('#show').click(function(){
+    $('#text').show();
+  });
+  $('#hide').click(function(){
+    $('#text').hide();
+  });
+});
